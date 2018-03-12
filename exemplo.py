2a49ef36f562175770d9982ac6b702e5c8d2125f
python-nyce <37301856+python-nyce@users.noreply.github.com>
2018-03-12 13:37:12 -0300

    Create exemplo.py

---
+++ exemplo.py
@@ -0,0 +1,9 @@
+# imports
+
+# Definindo as variaveis e pegando o CEP/ Ou o cep ja definido na variavel
+
+# Fazendo a requisicao
+
+# Tratando a requisicao (transformando o json para o retorno que queremos)
+
+# Retornando o JSON
